d1775cb2aee43964ef1ca0eaaa89032047fcc092
Jeremías Pérez Fernández <estige@gmail.com>
2022-02-02 17:35:35 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+      title: `What would you like to call your site?✔ · My First Gatsby Site`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+
+    ]
+}
